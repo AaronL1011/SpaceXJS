f3da0e7ee82a7688e9a18c9b4078242478cb52e6
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-06-05 20:49:54 +1000

    added initial dashboard

--- script.js
+++ script.js
@@ -1,82 +1,34 @@
-function getLatestMission() {
-  axios
-    .get('https://api.spacexdata.com/v3/launches/latest')
-    .then(function (response) {
-      const missionName = response.data.mission_name,
-        rocket = response.data.rocket,
-        launchDate = response.data.launch_date_utc,
-        details = response.data.details,
-        flightNumber = response.data.flight_number,
-        launchSuccess = response.data.launch_success,
-        upcoming = response.data.upcoming;
-      console.log(
-        `The latest mission was ${missionName}, flight number ${flightNumber}`
-      );
-      console.log(details);
-      if (upcoming) {
-        console.log(
-          `This launch is yet to happen, launch date is ${launchDate} UTC`
-        );
-      } else if (launchSuccess) {
-        console.log('This launch was a success!');
-      } else {
-        console.log('This launch may have gone wrong...');
-      }
-    });
+async function getRoadster() {
+  let res = await axios.get('https://api.spacexdata.com/v3/roadster');
+  let roadsterData = res.data;
+  setRoadsterInfo(roadsterData);
 }
 
-function getRoadster() {
-  axios.get('https://api.spacexdata.com/v3/roadster').then(function (response) {
-    const name = response.data.name,
-      details = response.data.details,
-      earthDistance = response.data.earth_distance_km,
-      marsDistance = response.data.mars_distance_km,
-      speed = response.data.speed_kph,
-      launchDate = response.data.launch_date_utc;
-
-    console.log(
-      `${name} is currently ${Math.floor(earthDistance)}km from earth!`
-    );
-    console.log(`Whizzing along at a whopping ${speed}Km/h`);
-    console.log(details);
-    console.log(`This payload was launched into space on ${launchDate} UTC`);
-  });
+function setRoadsterInfo(roadsterData) {
+  let myRoadster = {
+    name: roadsterData.name,
+    details: roadsterData.details,
+    earthDistance: Math.floor(roadsterData.earth_distance_km),
+    marsDistance: Math.floor(roadsterData.mars_distance_km),
+    speed: roadsterData.speed_kph,
+    launchDate: roadsterData.launch_date_utc
+  };
+  document.getElementById('title').innerHTML = myRoadster.name;
+  document.getElementById('description').innerHTML = myRoadster.details;
+  document.getElementById(
+    'distance-speed'
+  ).innerHTML = `Elon's Roadster is currently ${myRoadster.earthDistance}km from earth,
+   travelling a whopping ${myRoadster.speed}Km/h.`;
 }
 
-let spaceXShips = {};
-function getShips() {
-  axios.get('https://api.spacexdata.com/v3/ships').then(function (response) {
-    console.log(response.data);
-    for (ship of response.data) {
-      spaceXShips[ship.ship_name] = {
-        shipId: ship.ship_id,
-        homePort: ship.home_port,
-        model: ship.ship_model,
-        type: ship.ship_type,
-        image: ship.image,
-        yearBuilt: ship.year_built
-      };
-    }
+getRoadster();
 
-    for (let shipName in spaceXShips) {
-      console.log(shipName);
-    }
+// let myRoadster = getRoadster();
+// console.log(myRoadster);
 
-    let userChoice = prompt('Enter a ship name');
-    console.log(spaceXShips[userChoice]);
-  });
-}
-
-function getHistoricalEvents() {
-  axios.get('https://api.spacexdata.com/v3/history').then(function (response) {
-    console.log(response.data.reverse());
-    for (event of response.data) {
-      console.log(event.title);
-      console.log(event.event_date_utc);
-      console.log(event.details);
-      console.log('');
-    }
-  });
-}
-
-getHistoricalEvents();
+// const infoSection = document.querySelector('#info-section');
+// let infoElement = document.createElement('p');
+// for (i = 0; i <= 4; i++) {
+//   infoElement.innerText();
+//   infoSection.appendChild(infoElement);
+// }
